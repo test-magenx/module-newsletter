15a1f07c2d207d3db06fc389d3bcb85406f6c75d
Yegor Shytikov <egorshitiov@gmail.com>
2021-10-10 21:31:49 -0700

    Magento Fork initial commit

--- Block/Adminhtml/Template/Preview.php
+++ Block/Adminhtml/Template/Preview.php
@@ -65,6 +65,8 @@ class Preview extends \Magento\Backend\Block\Widget
             $template->setTemplateType($previewData['type']);
             $template->setTemplateText($previewData['text']);
             $template->setTemplateStyles($previewData['styles']);
+            // Emulate DB-loaded template to invoke strict mode
+            $template->setTemplateId(123);
         }
 
         \Magento\Framework\Profiler::start($this->profilerName);
@@ -84,7 +86,7 @@ class Preview extends \Magento\Backend\Block\Widget
         $template->revertDesign();
 
         if ($template->isPlain()) {
-            $templateProcessed = "<pre>" . htmlspecialchars($templateProcessed) . "</pre>";
+            $templateProcessed = "<pre>" . $this->escapeHtml($templateProcessed) . "</pre>";
         }
 
         \Magento\Framework\Profiler::stop($this->profilerName);
@@ -142,6 +144,8 @@ class Preview extends \Magento\Backend\Block\Widget
     }
 
     /**
+     * Return template
+     *
      * @param \Magento\Newsletter\Model\Template $template
      * @param string $id
      * @return $this
